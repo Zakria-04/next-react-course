023c77237907cb7e145a6a8f6c31e78af7b56abd
Zakria-04 <zakriaabdelgny04@gmail.com>
2024-07-19 00:06:35 +0300

    ...

--- src/assets/img/images.ts
+++ src/assets/img/images.ts
@@ -1,6 +1,7 @@
 const Images = {
   logo:() => require("./logo.png"),
   background1: () => require('./backround1.jpg'),
-  whatsApp: () => require('./whatsapp.png')
+  whatsApp: () => require('./whatsapp.png'),
+  codezoneBI1: () => require('./codezoneBI1.png')
 };
 export default Images;
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,7 +2,7 @@ import Images from "@/assets/img/images";
 import { url } from "inspector";
 import Image from "next/image";
 import React from "react";
-import "../styles/ContactForm.css"
+import "../styles/ContactForm.css";
 
 function ContactForm() {
   return (
@@ -11,11 +11,20 @@ function ContactForm() {
         <h2>دورة واحدة وانت في الهايتك!</h2>
         <h2>كورس برمجة تطبيقات ومواقع مهني شامل</h2>
         <p>من الصفر وحتى الاحتراف</p>
+        <Image
+          src={Images.codezoneBI1()}
+          alt="codezone"
+          className="codezoneBI1"
+          draggable={false}
+        />
       </div>
       <form>
         <div id="form_container">
           <p>
-            <span style={{color: '#FFC94A', fontSize: 23,}}> تبدأ الدورة في 01/09/2024</span>
+            <span style={{ color: "#FFC94A", fontSize: 23 }}>
+              {" "}
+              تبدأ الدورة في 01/09/2024
+            </span>
             <br />
             لجلسة استشارة مجانية
             <br />
